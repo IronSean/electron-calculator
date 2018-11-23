b97b0a99fc2ab057ddd82ea088dc3d27a3481a07
Sean <seanrmackay@gmail.com>
2018-11-22 20:53:50 -0400

    Basic view powered by CSS-grid

--- app/components/Calculator/Calculator.js
+++ app/components/Calculator/Calculator.js
@@ -1,3 +1,4 @@
+// @flow
 import React, { Component } from 'react';
 import CalculatorView from './CalculatorView';
 
--- app/components/Calculator/CalculatorView.js
+++ app/components/Calculator/CalculatorView.js
@@ -1,10 +1,49 @@
+// @flow
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Calculator.css';
+import routes from '../../constants/routes';
 
 type Props = {};
 
 export class CalculatorView extends Component<Props> {
   render() {
-    return 'This is my view';
+    return (
+      <div className={styles.container}>
+        <div className={styles.header} data-tid="backButton">
+          <Link to={routes.HOME}>
+            <i className="fa fa-arrow-left fa-3x" />
+          </Link>
+        </div>
+        <div
+          className={`calculator ${styles.calculator}`}
+          data-tid="calculator"
+        >
+          <div className={`${styles.calculatorDisplay}`}>
+            <div>12345</div>
+            <div>+ 322</div>
+          </div>
+          <div className={`${styles.calculatorButtons}`}>
+            <button className={`${styles.calculatorButton}`}>7</button>
+            <button className={`${styles.calculatorButton}`}>8</button>
+            <button className={`${styles.calculatorButton}`}>9</button>
+            <button className={`${styles.calculatorButton}`}>*</button>
+            <button className={`${styles.calculatorButton}`}>4</button>
+            <button className={`${styles.calculatorButton}`}>5</button>
+            <button className={`${styles.calculatorButton}`}>6</button>
+            <button className={`${styles.calculatorButton}`}>/</button>
+            <button className={`${styles.calculatorButton}`}>1</button>
+            <button className={`${styles.calculatorButton}`}>2</button>
+            <button className={`${styles.calculatorButton}`}>3</button>
+            <button className={`${styles.calculatorButton}`}>+</button>
+            <button className={`${styles.calculatorButton}`}>0</button>
+            <button className={`${styles.calculatorButton}`}>.</button>
+            <button className={`${styles.calculatorButton}`}>=</button>
+            <button className={`${styles.calculatorButton}`}>-</button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
